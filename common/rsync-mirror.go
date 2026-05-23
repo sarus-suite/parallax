06d83173264bbc0e4e08f5f72ebea84e4e5363ec
felipecr <cruz@cscs.ch>
2026-05-23 19:19:51 +0200

    relaxed rsync permission pattern

--- common/rsync-mirror.go
+++ common/rsync-mirror.go
@@ -1,12 +1,12 @@
 package common
 
 import (
-    "fmt"
-    "os"
-    "os/exec"
-    "path/filepath"
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
 
-    log "github.com/sirupsen/logrus"
+	log "github.com/sirupsen/logrus"
 )
 
 // Mirror creates a writable mirror of srcDir in a temp directory.
@@ -15,79 +15,81 @@ import (
 //
 // Note: this requires the "rsync" binary to be installed and in PATH.
 func Mirror(srcDir string) (mirrorDir string, cleanup func() error, err error) {
-    log.Infof("Mirror: creating temp dir for %q", srcDir)
+	log.Infof("Mirror: creating temp dir for %q", srcDir)
 
-    mp, err := os.MkdirTemp("", "rsync-mirror-")
-    if err != nil {
-        return "", nil, fmt.Errorf("Failed to create temp dir: %w", err)
-    }
+	mp, err := os.MkdirTemp("", "rsync-mirror-")
+	if err != nil {
+		return "", nil, fmt.Errorf("Failed to create temp dir: %w", err)
+	}
 
+	srcPath := filepath.Clean(srcDir) + string(os.PathSeparator)
+	mirrorPath := filepath.Clean(mp) + string(os.PathSeparator)
+	log.Infof("Mirror: rsync from %s to %s", srcPath, mirrorPath)
 
-    srcPath := filepath.Clean(srcDir) + string(os.PathSeparator)
-    mirrorPath := filepath.Clean(mp) + string(os.PathSeparator)
-    log.Infof("Mirror: rsync from %s to %s (no squash/)", srcPath, mirrorPath)
+	// Define PodmanOverlay file and directory patterns for mirror.
+	allowedPatterns := []string{
+		"overlay/***",
+		"overlay-containers/***",
+		"overlay-images/***",
+		"overlay-layers/***",
+		"squash/***",
+		"storage.lock",
+		"userns.lock",
+	}
 
-    // Define PodmanOverlay file and directory patterns for mirror
-    allowedPatterns := []string{
-        "overlay/***",
-        "overlay-containers/***",
-        "overlay-images/***",
-        "overlay-layers/***",
-        "storage.lock",
-        "userns.lock",
-    }
+	includePatterns := []string{}
+	for _, pattern := range allowedPatterns {
+		includePatterns = append(includePatterns, fmt.Sprintf("--include=%s", pattern))
+	}
 
-    includePatterns := []string{}
-    for _, pattern := range allowedPatterns {
-        includePatterns = append(includePatterns, fmt.Sprintf("--include=%s", pattern))
-    }
+	setupArgs := append([]string{
+		"-rltD",
+		"--no-owner",
+		"--no-group",
+		"--chmod=Du+rwx,Dgo+rx,Fu+rw,Fgo+r",
+	}, includePatterns...)
+	setupArgs = append(setupArgs, "--exclude=*", "--delete")
 
-	rsyncArgs := append([]string{"-a"}, includePatterns...)
-    rsyncArgs = append(rsyncArgs, "--exclude=*", "--delete")
+	log.Infof("Mirror setup: rsync from %s to %s", srcPath, mirrorPath)
+	rsyncCmd := append(setupArgs, srcPath, mirrorPath)
+	log.Infof("  rsync args %v", rsyncCmd)
+	cmd := exec.Command("rsync", rsyncCmd...)
 
-    log.Infof("Mirror setup: rsync from %s to %s", srcPath, mirrorPath)
-    rsyncCmd := append(rsyncArgs, srcPath, mirrorPath)
-    log.Infof("  rsync args %v", rsyncCmd)
-    cmd := exec.Command("rsync", rsyncCmd...)
+	if out, err2 := cmd.CombinedOutput(); err2 != nil {
+		os.RemoveAll(mp)
+		return "", nil, fmt.Errorf("Initial rsync failed: %v\n%s", err2, out)
+	}
 
-    if out, err2 := cmd.CombinedOutput(); err2 != nil {
-        os.RemoveAll(mp)
-        return "", nil, fmt.Errorf("Initial rsync failed: %v\n%s", err2, out)
-    }
+	if err := os.MkdirAll(filepath.Join(mirrorPath, "squash"), 0o755); err != nil {
+		os.RemoveAll(mp)
+		return "", nil, fmt.Errorf("Failed to create mirror squash dir: %w", err)
+	}
 
-    // In case srcDir is not init, we might not have a squash dir, lets create it
-    realSquash := filepath.Join(srcDir, "squash")
-    linkName   := filepath.Join(mirrorPath, "squash")
-    log.Infof("Mirror: creating squash symlink %s to %s", linkName, realSquash)
-    if err := os.MkdirAll(realSquash, 0o755); err != nil {
-        return "", nil, fmt.Errorf("Failed to create real squash dir %q: %w", realSquash, err)
-    }
-    if err2 := os.Symlink(realSquash, linkName); err2 != nil {
-        os.RemoveAll(mp)
-        return "", nil, fmt.Errorf("Squash symlink failed: %w", err2)
-    }
+	writeBackArgs := append([]string{
+		"-rltD",
+		"--no-owner",
+		"--no-group",
+		"--no-perms",
+		"--omit-dir-times",
+	}, includePatterns...)
+	writeBackArgs = append(writeBackArgs, "--exclude=*", "--delete")
 
-    // On cleanup we remove link then rsync back
-    cleanup = func() error {
-        log.Infof("Mirror-cleanup: remove mirror’s squash symlink")
-        if err := os.Remove(filepath.Join(mirrorPath, "squash")); err != nil {
-            return fmt.Errorf("Failed to remove squash symlink: %w", err)
-        }
+	// On cleanup we push the normalized working copy back without changing
+	// destination ownership or access policy.
+	cleanup = func() error {
+		log.Infof("Mirror-cleanup: rsync back from %s to %s", mirrorPath, srcPath)
+		rsyncCmd = append(writeBackArgs, mirrorPath, srcPath)
+		cmdBack := exec.Command("rsync", rsyncCmd...)
 
-        log.Infof("Mirror-cleanup: rsync back from %s to %s", mirrorPath, srcPath)
-        rsyncCmd = append(rsyncArgs, mirrorPath, srcPath)
-        cmdBack := exec.Command("rsync", rsyncCmd...)
+		if out, err2 := cmdBack.CombinedOutput(); err2 != nil {
+			return fmt.Errorf("rsync back failed: %v\n%s", err2, out)
+		}
 
-        if out, err2 := cmdBack.CombinedOutput(); err2 != nil {
-            return fmt.Errorf("rsync back failed: %v\n%s", err2, out)
-        }
+		if err2 := os.RemoveAll(mp); err2 != nil {
+			return fmt.Errorf("failed to remove temp dir %q: %w", mp, err2)
+		}
+		return nil
+	}
 
-        if err2 := os.RemoveAll(mp); err2 != nil {
-            return fmt.Errorf("failed to remove temp dir %q: %w", mp, err2)
-        }
-        return nil
-    }
-
-    return mp, cleanup, nil
+	return mp, cleanup, nil
 }
-
